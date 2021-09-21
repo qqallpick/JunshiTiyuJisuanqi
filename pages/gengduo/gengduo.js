1119f3e88b550d5bbbd719a152022e240335bb64
qqallpick <88521113@qq.com>
2021-09-21 11:19:01 +0800

    点赞逻辑修改
    点赞逻辑修改

--- pages/gengduo/gengduo.js
+++ pages/gengduo/gengduo.js
@@ -20,21 +20,26 @@ Page({
       url: '/pages/gongzhonghao/gongzhonghao',
     })
   },
+  kuaimi() {
+    wx.navigateToMiniProgram({
+      appId: 'wxe24f4563968e5f42',
+    })
+  },
   onLoad: function (options) {
     var that = this
     var scene_img = 'cloud://junshixunlianchengji-86wg6.6a75-junshixunlianchengji-86wg6-1303113678/zanshang.jpg' //这里添加图片的地址
     that.setData({
       scene: scene_img
     })
-    
+
   },
   zanshang() {
-    
-      wx.previewImage({
-        urls: this.data.scene.split(',')
-        // 需要预览的图片http链接  使用split把字符串转数组。不然会报错
-      })
-    
+
+    wx.previewImage({
+      urls: this.data.scene.split(',')
+      // 需要预览的图片http链接  使用split把字符串转数组。不然会报错
+    })
+
   },
   onShareAppMessage: function () {
     return {
@@ -54,10 +59,10 @@ Page({
     console.log('Banner 广告关闭')
   },
 
-  gaoyuanban(){
-wx.navigateToMiniProgram({
-  appId: 'wxd46e46c2d6d26383',
-})
+  gaoyuanban() {
+    wx.navigateToMiniProgram({
+      appId: 'wxd46e46c2d6d26383',
+    })
 
 
 
